2002e5a007ff2970727ff21484d8c7ee8331a8fd
aagrich <aagrich@gmail.com>
2016-05-16 16:08:29 +0300

    second variant win!

--- hw_js_1/js1.js
+++ hw_js_1/js1.js
@@ -1,48 +1,58 @@
 var a;
 var b;
-var arr;
 var result2;
 
 a = Math.abs(+prompt('Введите число', ''));
 b = Math.abs(+prompt('Введите степень', ''));
-arr = [a];
 
-function calculate( a, b) {
- for (var i = 1; i < b; i++) {
-  
-  var x;
-  var c;
-  
-   x = arr.length-1;
-   c=arr[x]*a;
-   arr.push(c);
+function pow(a, b) {
+  if (b == 1) {
+    return a;
   }
+  if (b == 0) {
+    return 1;
+  }
+  if (isNaN(a) || isNaN(b)) {
+    return "Введенные даные не являются числом! обновите страницу и введите числа!";
+  }
+  else var result = a * pow(a, b-1);
+  return result;
 }
- function pow() {
-  if ( b > 1) {
-     calculate( a , b );
-  }
-  if ( b === 1) {
-     arr.push(a);
-  }
-  if ( b < 1 & b > 0) {
-     arr.push(1/arr[b-1]);  //fix it later! (watch webinar)
-  }
-  //  if ( b === 0 || b === undefined || b === NaN) {
-  //    arr.push('0');
-  // }                       -  Dond forget!
- }
- 
- pow();
+result2 = pow(a, b);
+console.log( "Результатом ", a , " в " , b , " степени будет " , result2);
 
-console.log("Результатом ", a , " в " , b , " степени будет " , arr[b-1]);
 
-function test2(a, b) {
-  for (var i = 1; i < b; ++i) {
-    a = a*a;
-  }
-  return a;
-}
-result2 = test2(a,b);
-
-console.log( "second result will be = ", result2);
+//  
+//  
+//  var arr;
+//  arr = [a];
+//  
+//  function calculate( a, b) {
+//   for (var i = 1; i < b; i++) {
+//    
+//    var x;
+//    var c;
+//    
+//     x = arr.length-1;
+//     c=arr[x]*a;
+//     arr.push(c);
+//    }
+//  }
+//   function pow() {
+//    if ( b > 1) {
+//       calculate( a , b );
+//    }
+//    if ( b === 1) {
+//       arr.push(a);
+//    }
+//    if ( b < 1 & b > 0) {
+//       arr.push(1/arr[b-1]);  //fix it later! 
+//    }
+//    //  if ( b === 0 || b === undefined || b === NaN) {
+//    //    arr.push('0');
+//    // }                       -  Dond forget!
+//   }
+//   
+//   pow();
+//   
+//   console.log("Результатом ", a , " в " , b , " степени будет " , arr[b-1]);
