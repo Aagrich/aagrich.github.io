3b3708238b316b2616cbc21027844547a2cc4595
aagrich <aagrich@gmail.com>
2016-05-17 10:09:07 +0300

    add second variant

--- hw_js_2/js2.js
+++ hw_js_2/js2.js
@@ -1,28 +1,25 @@
 var numbersOfUsers;
 var listOfUsers;
 var x;
+var allow = ",  Вы успешно вошли";
 
-numbersOfUsers = 3;
+numbersOfUsers = 5;
 x = numbersOfUsers;
 listOfUsers = ["0"];
 
 
 
 function addUsers(x) {
-     for (var i = 0 ; i < x; i++) {
+     for ( i = 0 ; i < x; i++) {
           var userName;
           userName = prompt("Введите имя нового пользователя:", "");
           listOfUsers.push(userName);
-    
      }
 }
 function lookForUser() {
           var login;
-          var listLength;
           var result;
-          var allow = ",  Вы успешно вошли";
           login = prompt("Введите имя пользователя для входа", "");
-          listLength = listOfUsers.length;
           result = listOfUsers.indexOf(login, 0);
           if (result == -1) {
           return "Error!";
@@ -32,7 +29,13 @@ function lookForUser() {
           }
           return "Error!";
      }
-
+function findUser() {
+          login = prompt("Введите имя пользователя для входа", "");
+          for ( i = 0; i < x+1; i ++ ) {
+          if (listOfUsers[i] === login)
+               return listOfUsers[i] + allow;
+     }
+              return "Error!";
+     }
 addUsers(x);
-
-alert(lookForUser());
+alert(findUser());
