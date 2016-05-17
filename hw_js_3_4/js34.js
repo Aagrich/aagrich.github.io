25b7e51bb65b6a09f48ead1e8d5c59ed5b3da9a2
aagrich <aagrich@gmail.com>
2016-05-17 15:58:10 +0300

    some changes

--- hw_js_3_4/js34.js
+++ hw_js_3_4/js34.js
@@ -1,61 +1,93 @@
-var testpage = {
-	addElement: function(objects) {
-		var element = document.createElement(objects.tagName);
+// var testpage = {
+// 	addElement: function(objects) {
+// 		var element = document.createElement(objects.tagName);
+// 		if 
+// 
+// 	}
+// 	return element;
+// }
 
 
-	}
+var app = {
+
+  createElement: function(params) {
+    var element = document.createElement(params.tagName);
+
+    if (params.inputType){
+      element.setAttribute('type', params.inputType);
+    }
+
+    if (params.className){
+      element.className = params.className;
+    }
+
+    if (params.content){
+      element.innerHTML = params.content;
+    }
+
+    if (params.parentElement){
+      params.parentElement.appendChild(element);
+    }
+
+    return element;
+  },
+
+  generateQuestions: function(questionsAmount, answersAmount) {
+
+    for (var i = 0; i < questionsAmount; i++) {
+
+      this.createElement({
+        tagName: 'h2',
+        content: 'Вопрос №' + (i + 1),
+        parentElement: form
+      });
+
+      for (var j = 0; j < answersAmount; j++) {
+
+        var label = this.createElement({
+          tagName: 'label',
+          content: 'Вариант ответа №' + (j + 1),
+          parentElement: form
+        });
+
+        var checkbox = this.createElement({
+          tagName: 'input',
+          inputType: 'checkbox'
+        });
+
+        label.insertAdjacentElement('afterBegin', checkbox);
+      }
+
+    };
+
+  }
+
 }
 
 
 
+var body = document.querySelector('body');
 
 
+app.createElement({
+  tagName: 'h1',
+  content: 'Тест по программированию',
+  parentElement: body
+});
 
+var form = app.createElement({
+  tagName: 'form',
+  parentElement: body
+});
 
+app.generateQuestions(3, 3);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.createElement({
+  tagName: 'input',
+  inputType: 'submit',
+  content: 'Проверить мои результаты',
+  parentElement: form
+});
 
 
 
@@ -125,3 +157,4 @@ question3.insertBefore(variant7, null);
 question3.insertBefore(variant8, null);
 question3.insertBefore(variant9, null);
 page.insertBefore(button, null);
+*/
