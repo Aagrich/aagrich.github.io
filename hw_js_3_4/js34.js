2ffc034a401c8319d16811ccd2817c3260186c28
aagrich <aagrich@gmail.com>
2016-05-18 13:13:17 +0300

    almost done (50%)

--- hw_js_3_4/js34.js
+++ hw_js_3_4/js34.js
@@ -1,93 +1,40 @@
-// var testpage = {
-// 	addElement: function(objects) {
-// 		var element = document.createElement(objects.tagName);
-// 		if 
-// 
-// 	}
-// 	return element;
-// }
-
-
-var app = {
-
-  createElement: function(params) {
-    var element = document.createElement(params.tagName);
-
-    if (params.inputType){
-      element.setAttribute('type', params.inputType);
+ var testPage = {
+  tagPosition: null,
+ 	addElement: function(task) {
+    var element = document.createElement(task.tag);
+   
+    if (task.tagClass){
+      task.className = task.tagClass;
     }
-
-    if (params.className){
-      element.className = params.className;
+    if (task.tagType){
+      task.setAttribute('type', task.tagType);
     }
-
-    if (params.content){
-      element.innerHTML = params.content;
+    if (task.tagValue){
+      task.setAttribute('value', task.tagValue);
     }
-
-    if (params.parentElement){
-      params.parentElement.appendChild(element);
+    if (task.tagContent){
+      element.innerHTML = task.tagContent;
     }
-
+    if (task.tagParent){
+      task.tagParent.insertBefore(element, task.tagPosition);
+    }
     return element;
-  },
-
-  generateQuestions: function(questionsAmount, answersAmount) {
-
-    for (var i = 0; i < questionsAmount; i++) {
-
-      this.createElement({
-        tagName: 'h2',
-        content: 'Вопрос №' + (i + 1),
-        parentElement: form
-      });
-
-      for (var j = 0; j < answersAmount; j++) {
-
-        var label = this.createElement({
-          tagName: 'label',
-          content: 'Вариант ответа №' + (j + 1),
-          parentElement: form
-        });
-
-        var checkbox = this.createElement({
-          tagName: 'input',
-          inputType: 'checkbox'
-        });
-
-        label.insertAdjacentElement('afterBegin', checkbox);
-      }
-
-    };
-
-  }
-
+ }
 }
-
-
-
 var body = document.querySelector('body');
 
 
-app.createElement({
-  tagName: 'h1',
-  content: 'Тест по программированию',
-  parentElement: body
+testPage.addElement({
+  tag: 'div',
+  tagParent: body
 });
+var div = document.querySelector('div');
+testPage.addElement({
+  tag: 'p',
+  tagContent: 'Some text',
+  tagParent: div
+  });
 
-var form = app.createElement({
-  tagName: 'form',
-  parentElement: body
-});
-
-app.generateQuestions(3, 3);
-
-app.createElement({
-  tagName: 'input',
-  inputType: 'submit',
-  content: 'Проверить мои результаты',
-  parentElement: form
-});
 
 
 
