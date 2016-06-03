02029b76e03d7c97279db786f01ae61047e5f05e
aagrich <aagrich@gmail.com>
2016-06-03 16:40:36 +0300

    start second work

--- hw_js_7_8/js/js78.js
+++ hw_js_7_8/js/js78.js
@@ -8,11 +8,16 @@ $( document ).ready(function() {
 // var $animat = $(this).animate({opacity: 1}, 5000, $(this).find('p').css('display', 'inline'));
 function $showMessage() {
 	$(this).find('p').css('display', 'inline');
- 	}
+ 	};
 function $hideMessage() {
 	$(this).find('p').css('display', 'none');
- 	}
+ 	};
+function $showText() {
+	
+	};
+function $hideText() {
 
+	};
 
  $firstname.hover($showMessage,$hideMessage);
  $lastname.hover($showMessage,$hideMessage); 
