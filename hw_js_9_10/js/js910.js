616708852515397a31c3c51a207276b2b0bdc760
Aagrich <aagrich@gmail.com>
2016-06-08 21:14:48 +0300

    some changes

--- hw_js_9_10/js/js910.js
+++ hw_js_9_10/js/js910.js
@@ -1,8 +1,12 @@
 'use strict'
 
 $(function() {
-    $('.jcarousel').jcarousel();
-    $('.jcarousel-prev').click(function(){
+    $('.jcarousel').jcarousel({
+      vertical: true
+    });
+    $('.jcarousel-next').click(function(){
       $('.jcarousel').jcarousel('scroll', '+=1');
+      $('.jcarousel-prev').click(function(){
+      $('.jcarousel').jcarousel('scroll', '-=1');
     })
 });
