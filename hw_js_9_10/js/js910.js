8fe5a060fce37642f236e55ba3edbf4d1e1375ed
aagrich <aagrich@gmail.com>
2016-06-09 11:28:54 +0300

    add jquery files

--- hw_js_9_10/js/js910.js
+++ hw_js_9_10/js/js910.js
@@ -1,23 +1,15 @@
-'use strict'
+$(function(){
 
-$(function() {
-    $('.jcarousel')
-        .jcarousel({
-              rtl: true
-        })
-        .jcarouselAutoscroll({
-            interval: 3000,
-            target: '+=1',
-            autostart: true
-        })
-    ;
-    $('.jcarousel-prev').click(function() {
-    $('.jcarousel').jcarousel('scroll', '-=1');
-});
+	$('.jcarousel').jcarousel();
 
-$('.jcarousel-next').click(function() {
-    $('.jcarousel').jcarousel('scroll', '+=1');
-});
+	$('.jcarousel-next').click(
+		function(){
+			$('.jcarousel').jcarousel( "scroll", '+=1');
+		});
+	$('.jcarousel-prev').click(
+		function(){
+			$('.jcarousel').jcarousel( "scroll", '-=1');
+		});
 
 
 });
