7aea242af623e6c9f7a2ab6247767b5a82c86635
aagrich <aagrich@gmail.com>
2016-06-16 14:23:52 +0300

    try another variant

--- hw_js_9_10/js/js910_2.js
+++ hw_js_9_10/js/js910_2.js
@@ -1,14 +1,46 @@
-$(function(){
-	var checkbox = $('div.wrapper2 input');
-	checkbox.css({'float' :'right',
-				  'margin':'0',
-				  'display': 'none'});
-	checkbox.after("<div class='pic'></div>");
-	$('div.pic').css({
-		'display': 'block',
-		'float'  : 'right',
-		'width'  : '17px',
-		'height' : '17px',
-		'background-image': 'url(img/checkbox.png)'
-	});
+
+
+$(function() {
+
+function jcheckbox(elem) {
+	var $elem = $(elem);
+	var $checkbox = $("input", $elem);
+	console.log($checkbox);
+};
+
+jcheckbox('div.wrapper2');
+
 });
+
+
+
+
+
+// $(function(){
+// 	var checkbox = $('div.wrapper2 input');
+// 
+// 	checkbox.css({
+// 		'float' :'right',
+// 		'margin':'0',
+// 		'display': 'none'});
+// 
+// 	checkbox.after("<div class='pic'></div>");
+// 
+// 	$('div.pic').css({
+// 		'display': 'block',
+// 		'float'  : 'right',
+// 		'width'  : '17px',
+// 		'height' : '17px',
+// 		'background-image': 'url(img/checkbox.png)'
+// 	});
+// 
+// 	$('div.pic').click(function() {
+// 	if ($(this).prop('checked')) {
+// 		alert('a');
+// 	};
+// 	{
+// 		$(this).attr('checked');
+// 	};
+// 	});
+// });
+
