34618bc169ebf967ee64d513725473502e0313a8
aagrich <aagrich@gmail.com>
2016-06-16 14:35:21 +0300

    add var css element

--- hw_js_9_10/js/js910_2.js
+++ hw_js_9_10/js/js910_2.js
@@ -6,6 +6,24 @@ function jcheckbox(elem) {
 	var $elem = $(elem);
 	var $checkbox = $("input", $elem);
 	console.log($checkbox);
+	var $noNchecked = {
+		display: 'block',
+ 		float  : 'right',
+ 		width  : '17px',
+ 		height : '17px',
+ 		backgroundImage: 'url(img/checkbox.png)'
+	};
+	var $checked = {
+
+	};
+
+	$checkbox.css({
+		'float'  : 'right',
+		'margin' : '0',
+		'display': 'none'});
+	$checkbox.after("<div class='pic'></div>");
+
+
 };
 
 jcheckbox('div.wrapper2');
