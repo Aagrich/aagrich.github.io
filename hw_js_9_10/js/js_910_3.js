1c9ee5db0c04cc1309d2da9b8e1b3feff8e105cc
aagrich <aagrich@gmail.com>
2016-06-17 15:13:30 +0300

    some changes

--- hw_js_9_10/js/js_910_3.js
+++ hw_js_9_10/js/js_910_3.js
@@ -1,9 +1,25 @@
 $(function(){
 
-	function(mainMenu,secondMenu,lastMenu) {
-		var $mainMenu = $(mainMenu);
+	function jmenu(mainMenu, secondMenu, lastMenu) {
+		var $mainMenu   = $(mainMenu);
 		var $secondMenu = $(secondMenu);
-		var $lastMenu = $(lastMenu);
-	}
+		var $lastMenu   = $(lastMenu);
+
+		$secondMenu.css(
+			{'height': '0',
+			 'overflow': 'hidden'
+			});
+		$lastMenu.css({
+			'height': '0',
+			'overflow': 'hidden'
+			});
+
+		
+
+
+	
+	};
+
+	jmenu('.main-menu', '.second-menu', '.last-menu');
 
 });
