e90ef164b6bc7c2d6db4ae8dc428f3d9e04d4b2c
Aagrich <aagrich@gmail.com>
2016-06-19 17:42:02 +0300

    use slide method

--- hw_js_9_10/js/js_910_3.js
+++ hw_js_9_10/js/js_910_3.js
@@ -4,21 +4,36 @@ $(function(){
 		var $mainMenu   = $(mainMenu);
 		var $secondMenu = $(secondMenu);
 		var $lastMenu   = $(lastMenu);
+		var $mainInner  = $(mainMenu+ " li:nth-child(2)");
+		var $secondInner= $(secondMenu+ " li:nth-child(2)");
 
-		$secondMenu.css(
-			{'height': '0',
-			 'overflow': 'hidden'
+		$secondMenu.appendTo($mainInner);
+		$lastMenu.appendTo($secondInner);
+
+		$mainMenu.css({
+			'position': 'relative'
+		});
+		$secondMenu.css({
+			'position': 'absolyte',
+			'display': 'none'
 			});
 		$lastMenu.css({
-			'height': '0',
-			'overflow': 'hidden'
+			'position': 'absolyte',
+			'display': 'none'
 			});
 
-		
+		$mainInner.hover(function(){
+			$secondMenu.slideDown(200);
+		}, function(){
+			$secondMenu.slideUp(200);
+		});
+		$secondInner.hover(function(){
+			$lastMenu.slideDown(200);
+		}, function(){
+			$lastMenu.slideUp(200);
+		});
 
-
-	
-	};
+};
 
 	jmenu('.main-menu', '.second-menu', '.last-menu');
 
