aedafc9e56eb3e7d8cee9ba04d73d7070e03efbd
Aagrich <aagrich@gmail.com>
2016-06-27 17:15:39 +0300

    add miltitest! need to make modal window!

--- js_13_14/js/js13.js
+++ js_13_14/js/js13.js
@@ -10,7 +10,7 @@ $(function(){
          'Jack',
          'Mike'
      ],
-        answer: ['Mike', 'John']
+        answer: ['John', 'Mike']
      },
      {
        name: 'Where?',
@@ -34,6 +34,8 @@ $(function(){
      }
    ];
 
+
+
    var content = tmpl( $testList, {questiong});
 
    $('ol').append(content).jCheckbox('left');
@@ -41,23 +43,39 @@ $(function(){
    $('button').click(function(){
 
       var selected = $('.selected');
+      var key = true;
+      var test = false;
+      var allAnswers = true;
+      var answers = [];
+      var userAnswers = [];
 
+      for (i = 0; i < questiong.length; i++) {
+          for (y = 0; y < questiong[i].answer.length; y++) {
+            answers.push(questiong[i].answer[y]);
+        }
+      }
+
+      // TEST CHECKER
       for (i = 0; i < selected.length; i++) {
             var question = selected[i].getAttribute('name');
             var answer = selected[i].getAttribute('value');
-            console.log(question + '   ' + answer);
+
+            userAnswers.push(answer);
+
             for (y = 0; y < questiong.length ; y++) {
                 var name = questiong[y];
                 if (questiong[y].name === question) {
                     var result = questiong[y].answer.indexOf(answer);
                     if (result > -1) {
-                        console.log(answer + '  is RIGHT!');
+                         test = true;
                     }
                     else {
-                        console.log(answer + ' is WRONG!');
+                         key = false;
                     }
                 };
+
             }
+
 //          var result;
 //          var x = 0;
 //          var q = selected[i].matches('input[name="'+questiong[0].name+'"]');
@@ -70,6 +88,21 @@ $(function(){
 //            alert('wrong');
 //          }
       }
+      for (i = 0; i < answers.length ; i ++)  {
+          if (userAnswers.indexOf(answers[i]) === -1) {
+              allAnswers = false;
+          }
+      }
+      if ( key & test & allAnswers) {
+          console.log(' GOOD RESULT!');
+          console.log('RIGHT '+ answers);
+          console.log('Your answers ' + userAnswers);
+      }
+      else {
+          console.log(' BAD, VERY BAD!');
+          console.log('RIGHT '+ answers);
+          console.log('Your answers ' + userAnswers);
+      };
  });
 
 
