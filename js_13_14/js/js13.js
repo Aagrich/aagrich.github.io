3623cef8f8ea79a2177599831c6adc4d13b772d2
Aagrich <aagrich@gmail.com>
2016-06-28 01:17:03 +0300

    start add message

--- js_13_14/js/js13.js
+++ js_13_14/js/js13.js
@@ -34,8 +34,6 @@ $(function(){
      }
    ];
 
-
-
    var content = tmpl( $testList, {questiong});
 
    $('ol').append(content).jCheckbox('left');
@@ -43,8 +41,6 @@ $(function(){
    $('button').click(function(){
 
       var selected = $('.selected');
-//      var key = true;
-//      var test = false;
       var allAnswers = true;
       var answers = [];
       var userAnswers = [];
@@ -54,39 +50,10 @@ $(function(){
             answers.push(questiong[i].answer[y]);
         }
       }
-
-      // TEST CHECKER
       for (i = 0; i < selected.length; i++) {
             var question = selected[i].getAttribute('name');
             var answer = selected[i].getAttribute('value');
-
             userAnswers.push(answer);
-
-//            for (y = 0; y < questiong.length ; y++) {
-//                var name = questiong[y];
-//                if (questiong[y].name === question) {
-//                    var result = questiong[y].answer.indexOf(answer);
-//                    if (result > -1) {
-//                         test = true;
-//                    }
-//                    else {
-//                         key = false;
-//                    }
-//                };
-//
-//            }
-//
-//          var result;
-//          var x = 0;
-//          var q = selected[i].matches('input[name="'+questiong[0].name+'"]');
-//          var a = selected[i].matches('input[value="'+questiong[0].variant[0]+'"]');
-//          if(q & a) {
-//            var result = true;
-//              alert('Good!');
-//           }
-//          else {
-//            alert('wrong');
-//          }
       }
       for (i = 0; i < answers.length ; i ++)  {
           if (userAnswers.indexOf(answers[i]) === -1) {
@@ -96,18 +63,17 @@ $(function(){
               allAnswers = false;
           }
       }
-//      if ( key & test & allAnswers) {
     if (allAnswers) {
           console.log(' GOOD RESULT!');
-          console.log('RIGHT '+ answers);
-          console.log('Your answers ' + userAnswers);
+
       }
       else {
           console.log(' BAD, VERY BAD!');
-          console.log('RIGHT '+ answers);
-          console.log('Your answers ' + userAnswers);
       };
  });
-
+ function mWindow(message) {
+     var result;
+     body.innerHtml(result);
+ }
 
 });
