2b02f0cdb34e2a5f1732bf06a83de146265c08b4
aagrich <aagrich@gmail.com>
2016-07-01 16:29:09 +0300

    need to align url and pic

--- hw_js_15_16/js/mainscript.js
+++ hw_js_15_16/js/mainscript.js
@@ -9,17 +9,24 @@ $('#myLuck').click(function(){
 
 $('#search').click(function(){
 	var ask = $('.search')[0].value;
-	$.getJSON("https://api.riffsy.com/v1/search?key=LIVDSRZULELA&tag=" + ask, 
+	$.getJSON("https://api.riffsy.com/v1/search?key=LIVDSRZULELA&tag=" + ask + "&limit=15", 
 	function(data){
+		$('.results > div').remove();
+		$('.wrapper').css({
+			'margin': '5%',
+			'text-align': 'left',
+		});
+		$('input').css('display','inline-block');
+		$('#myLuck').css('display','none');
+		$('#search').css('display','inline-block');
+		$('.logo').css('margin', '15px');
 		var dataLength = data.results.length;
-
 		for (i = 0; i < dataLength; i++ ) {
 		var result = data.results[i];
 		var element = document.createElement('div');
-		element.innerHTML = result.title + '<a href=' + result.url + '>' + '<img src=' + result.url + '>' + '</a>';
-		list.appendChild(element);
+		element.innerHTML = result.title + '<a class="pic" href=' + result.url + '>' + '<img src=' + result.url + '>'+ '<br>' + result.url + '</a>';
+		$('.results').append(element);
 		};
-		$('.results').html(list);	
 	});
    });
 });
