2d06ae7b0faae1e0c011cac5f0602da796f04752
aagrich <aagrich@gmail.com>
2016-07-01 11:13:12 +0300

    css list left

--- hw_js_15_16/js/mainscript.js
+++ hw_js_15_16/js/mainscript.js
@@ -11,15 +11,14 @@ $('#search').click(function(){
 	var ask = $('.search')[0].value;
 	$.getJSON("https://api.riffsy.com/v1/search?key=LIVDSRZULELA&tag=" + ask, 
 	function(data){
-		console.log('result = ' + data);
 		var dataLength = data.results.length;
-		var x = data.results[1];
-		console.log(x);
-		console.log(dataLength);
 		var list = document.createElement('ul');
+		for (i = 0; i < dataLength; i++ ) {
+		var result = data.results[i];
 		var listItem = document.createElement('li');
-		listItem.innerHTML = x.title;
+		listItem.innerHTML = result.title + '<a href=' + result.url + '>' + '<img src=' + result.url + '>' + '</a>';
 		list.appendChild(listItem);
+		};
 		$('.results').html(list);	
 	});
    });
