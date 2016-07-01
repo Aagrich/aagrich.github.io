05bc098ebd95ced760e3728f57f8908d2a453b7d
aagrich <aagrich@gmail.com>
2016-07-01 14:55:31 +0300

    need add pic as background

--- hw_js_15_16/js/mainscript.js
+++ hw_js_15_16/js/mainscript.js
@@ -12,12 +12,12 @@ $('#search').click(function(){
 	$.getJSON("https://api.riffsy.com/v1/search?key=LIVDSRZULELA&tag=" + ask, 
 	function(data){
 		var dataLength = data.results.length;
-		var list = document.createElement('ul');
+
 		for (i = 0; i < dataLength; i++ ) {
 		var result = data.results[i];
-		var listItem = document.createElement('li');
-		listItem.innerHTML = result.title + '<a href=' + result.url + '>' + '<img src=' + result.url + '>' + '</a>';
-		list.appendChild(listItem);
+		var element = document.createElement('div');
+		element.innerHTML = result.title + '<a href=' + result.url + '>' + '<img src=' + result.url + '>' + '</a>';
+		list.appendChild(element);
 		};
 		$('.results').html(list);	
 	});
