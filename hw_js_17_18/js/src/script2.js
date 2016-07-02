b0d4e7a7a2af7dc74c39e3c692dbe155410fa1a5
Aagrich <aagrich@gmail.com>
2016-07-02 16:59:25 +0300

    add hw_js directory with files

---
+++ hw_js_17_18/js/src/script2.js
@@ -0,0 +1,11 @@
+var man = {
+    name: "John",
+    secondName: "Snow",
+    age: 27,
+}
+
+console.log(man.name);
+
+var actor = man.name + " " + man.secondName;
+
+console.log(actor);
---
+++ hw_js_17_18/js/src/script3.js
@@ -0,0 +1,13 @@
+var arr = [
+    'first',
+    'second',
+    'friend',
+    'enemy',
+    'full'
+];
+
+function() {
+    for (i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+    }
+};
