4ac946869c025e83e317cd00907783f4bfc89ff9
Aagrich <aagrich@gmail.com>
2016-07-09 13:15:01 +0300

    make watch with sass

--- hw_js_19_20/gruntfile.js
+++ hw_js_19_20/gruntfile.js
@@ -11,10 +11,21 @@ module.exports = function(grunt) {
   				'css/style.css': 'dev/css/*.scss'
   			}
   		}
-  	}
+  	},
+    watch: {
+      sass: {
+        files: ["dev/css/*.scss"],
+        tasks: ['scssToCss'],
+      }
+    }
   });
 
   grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('watchOn', ['watch']);
+
+  grunt.registerTask('scssToCss', ['sass']);
 
   grunt.registerTask('default', ['sass']);
 
