889a71d22c5d681db22ee50f3de8c9e0da74f768
aagrich <aagrich@gmail.com>
2016-07-22 14:22:44 +0300

    play with grunt and sass import

--- KAI/gruntfile.js
+++ KAI/gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
   		},
   	dist: {
   			files: {
-  				'css/style.css': 'dev/css/*.scss'
+  				'css/style.css': 'dev/css/style.scss'
   			}
   		}
   	},
@@ -17,6 +17,10 @@ module.exports = function(grunt) {
           src: ['dev/js/*.js'],
           dest: 'dev/script.main.js',
         },
+//        css: {
+//          src: ['dev/*.css'],
+//          dest: 'css/style.css',
+//        },
       },
     uglify: {
       dist: {
@@ -28,18 +32,34 @@ module.exports = function(grunt) {
           sass: {
             files: ["dev/css/*.scss"],
             tasks: ['scssToCss'],
+          },
+          concat: {
+            files: ['dev/js/*.js'],
+            tasks: ['concatJS'],
+          },
+ //         concat: {
+ //           files: ['dev/*.css'],
+ //           tasks: ['concatJS'],
+ //         },
+          uglify: {
+            files: ['dev/*.js'],
+            tasks: ['uglifyJS'],
           }
         }
   });
 
   grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks("grunt-contrib-concat");
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.registerTask('watchOn', ['watch']);
 
   grunt.registerTask('scssToCss', ['sass']);
+  grunt.registerTask('concatJS', ['concat']);
+  grunt.registerTask('uglifyJS', ['uglify']);
 
-  grunt.registerTask('default', ['sass']);
+  grunt.registerTask('default', ['sass' , 'concat', 'uglify' ]);
 
 
 
