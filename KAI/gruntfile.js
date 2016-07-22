e312c06f6349d36faaef51093b154d3257c94eda
aagrich <aagrich@gmail.com>
2016-07-22 11:01:34 +0300

    add concat, uglify

--- KAI/gruntfile.js
+++ KAI/gruntfile.js
@@ -6,18 +6,30 @@ module.exports = function(grunt) {
   		options: {
   			sourceMap: true
   		},
-  		dist: {
+  	dist: {
   			files: {
   				'css/style.css': 'dev/css/*.scss'
   			}
   		}
   	},
+    concat: {
+        dist: {
+          src: ['dev/js/*.js'],
+          dest: 'dev/script.main.js',
+        },
+      },
+    uglify: {
+      dist: {
+        src: ['dev/script.main.js'],
+        dest: 'js/script.js',
+        },
+      },
     watch: {
-      sass: {
-        files: ["dev/css/*.scss"],
-        tasks: ['scssToCss'],
-      }
-    }
+          sass: {
+            files: ["dev/css/*.scss"],
+            tasks: ['scssToCss'],
+          }
+        }
   });
 
   grunt.loadNpmTasks('grunt-sass');
