5fbe676f231425de3dc49bcac1083bb1537109d8
aagrich <aagrich@gmail.com>
2016-08-10 16:44:20 +0300

    add cheker and modal window

--- goIT/hw_js_21_22/js/script.js
+++ goIT/hw_js_21_22/js/script.js
@@ -53,13 +53,37 @@ $(function(){
   		   page.appendChild(test);
   };
   function checkTest(test) {
-  			let rightAnswers = function(){
+
+  			let answers = function(){
   				for (let question of test) {
   					for (let value of test.question.answer) {
-  						rightAnswers.push(value);
+  						Answers.push(value);
   					}
   				}
+  				return Answers;
   			};
-  }
+  			let userAnswers = querySelectorsAll('.selected');
+
+  			for (let value of answers) {
+  				if (userAnswers.indexOf(value) == -1) {
+  					return false;
+  				};
+  				return true;
+  			};
+  };
+  function showResult(result) {
+  	let page = document.getElementById(pageId);
+  	let message;
+  		if (result === true) {
+  			message = "Good RESULT!";
+  		};
+  		else {
+  			message = "BAD result!";
+  		};
+  		let modalWindow = `${'<div class = window-wrapper><div class = modal-window><p>'} + 
+  		${message} + 
+  		${'</p></div> </div>'} + ${'<button class=OKbutton> OK </button>'}`;
+  		page.appendChild(modalWindow);
+  };
 
 });
