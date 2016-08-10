b232d4a19cd96b0376dbc72b9fbd9eefb668bd07
aagrich <aagrich@gmail.com>
2016-08-10 13:19:50 +0300

    add func for test create

--- goIT/hw_js_21_22/js/script.js
+++ goIT/hw_js_21_22/js/script.js
@@ -33,4 +33,33 @@ $(function(){
      }
    ];
 
+
+
+
+   var makeTest = function(test) {
+   		   let testList = createElement('ol');
+   		for (let question of test) {
+   			testList.innerHTML(`${'<li>'} + ${question.name} + ${ for (let variant of question.options) {
+   				return `
+   				${'<label>'} + 
+   				${'<input name='} + ${question.name} + ${'type=checkbox'} + ${'value='} + ${variant} +${'>'} +
+   				${variant} + ${'</label>'}`;
+   			};}`);
+   			return testList;
+   		}
+   };
+  function pushTest(test, pageId) {
+  		   let page = document.getElementById(pageId);
+  		   page.appendChild(test);
+  };
+  function checkTest(test) {
+  			let rightAnswers = function(){
+  				for (let question of test) {
+  					for (let value of test.question.answer) {
+  						rightAnswers.push(value);
+  					}
+  				}
+  			};
+  }
+
 });
