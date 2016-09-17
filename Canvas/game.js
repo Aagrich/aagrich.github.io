0cf5bc6d544fa1f486f7a594fe34edd8054d0dfe
Aagrich <aagrich@gmail.com>
2016-09-18 02:11:38 +0300

    start own little test-project

---
+++ Canvas/game.js
@@ -0,0 +1,7 @@
+var canvas = document.getElementById('gameWindow');
+var c = canvas.getContext('2d');
+
+c.fillStyle = 'lightblue';
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+console.log(canvas);
