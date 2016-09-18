441ad3ac109d9de3a7cf080db244b87b1ac36440
Aagrich <aagrich@gmail.com>
2016-09-18 10:44:41 +0300

    play with  circle)

--- Canvas/game.js
+++ Canvas/game.js
@@ -4,4 +4,42 @@ var c = canvas.getContext('2d');
 c.fillStyle = 'lightblue';
 c.fillRect(0, 0, canvas.width, canvas.height);
 
-console.log(canvas);
+c.fillStyle = 'yellow';
+    c.beginPath();
+    var x = 100;
+    var y = 100;
+
+
+
+document.addEventListener("keydown", function(event) {
+  console.log(event.keyCode);
+
+    if (event.keyCode == 37) {
+      x = x - 5;
+    };
+
+    if (event.keyCode == 39) {
+      x = x + 5;
+    };
+
+    c.moveTo(x, y);
+c.arc( x, y, 10, 100, 200, false);
+c.fill();
+});
+document.addEventListener("keydown", function(event) {
+  console.log(event.keyCode);
+
+    if (event.keyCode == 38) {
+      y = y - 5;
+    };
+
+    if (event.keyCode == 40) {
+      y = y + 5;
+    };
+    c.moveTo(x, y);
+c.arc( x, y, 10, 100, 200, false);
+c.fill();
+});
+
+
+
