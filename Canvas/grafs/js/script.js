0e70f0d1e145220669734aae1d533c032f2c0752
aagrich <aagrich@gmail.com>
2016-09-27 13:59:29 +0300

    reorganize js code

--- Canvas/grafs/js/script.js
+++ Canvas/grafs/js/script.js
@@ -1,46 +1,41 @@
 
-var sails = [15,88,45,35,28,17,24,46,67,50,32,47,60];
+var sails = [1,32,64,92,28,56,24,46,67,50,101,47,60];
 
 function makeList(data,listId) {
+    var delButton = document.createElement('button');
+    delButton.innerHTML = 'X';
     var list = document.getElementById(listId);
     for (var i = 0; i < data.length; i++) {
         var punct = document.createElement('li');
-        punct.innerHTML = data[i];
+        if (data[i] > 1) {
+            punct.innerHTML = data[i] + " sails";
+        };
+        if (data[i] == 1) {
+            punct.innerHTML = data[i] + " sail";
+        }
+        punct.appendChild(delButton);
         list.appendChild(punct);
     }
 };
 
-makeList(sails,'sailsList');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+function makeDiagram(data) {
 
 var canvas = document.getElementById('canvas');
  var c = canvas.getContext('2d');
  c.fillStyle = 'lightgray';
- c.fillRect(0,0,500,500);
+ c.fillRect(0,0,canvas.height,canvas.width);
 
  var x = 0;
  var y = 0;
- for (var i = 0; i < sails.length; i++){
-     x += 500/sails.length-4;
-     y = 500 - sails[i];
-     var z = 500/sails.length-4;
+ for (var i = 0; i < data.length; i++){
+     x += canvas.width/data.length-4;
+     y = canvas.width - data[i];
+     var z = canvas.width/data.length-4;
      c.fillStyle = 'black';
      c.beginPath();
      c.moveTo(x, y);
-     c.lineTo( z + x, 500-sails[i+1]);
+     c.lineTo( z + x, canvas.width - sails[i+1]);
      c.stroke();
      c.beginPath();
      c.moveTo(x, y);
@@ -48,3 +43,7 @@ var canvas = document.getElementById('canvas');
      c.fill();
      c.stroke();
  };
+};
+
+makeList(sails,'sailsList');
+makeDiagram(sails);
