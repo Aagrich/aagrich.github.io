0b4b435c916b1037677595b966c43e25f37520a6
aagrich@gmail.com <aagrich@gmail.com>
2016-10-25 16:16:33 +0300

    add slider and animation

--- Empotools/script.js
+++ Empotools/script.js
@@ -2,15 +2,19 @@
     var goalStatus = false;   
     var el = document.getElementById('catalog');
     var goal = document.getElementById('goodsCatalog');
+    var topSlider = document.getElementById('topSlider');
     function showMenu() {
         if (goalStatus === true) {
             goalStatus = false;
-            Velocity( goal, {height : 0}, 1000);
+            Velocity( goal, {height : 0}, 700);
+            if (topSlider) {Velocity( topSlider, {'margin-left' : '125px'}, 1150);
+                            }
             return;
         };
         if (goalStatus === false) {
             goalStatus = true;
             Velocity( goal, {height : '520px'}, 1000);
+            if (topSlider) Velocity( topSlider, {'margin-left' : '250px'}, 600);
         };
 
     };
